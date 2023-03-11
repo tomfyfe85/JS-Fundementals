165b1ef71ced5a52ebc8d0b42bbd332a86c65571
Tom Fyfe <tomfyfe85@gmail.com>
2023-03-11 18:56:28 +0000

    need to add array to pokedex

--- phase2/my-project/part5/api.js
+++ phase2/my-project/part5/api.js
@@ -1,7 +1,12 @@
 const fetchPokemon = (name) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => ({
+      name: data.name,
+      id: data.id,
+      height: data.height,
+      weight: data.weight,
+    }));
 };
 
 module.exports = fetchPokemon;
