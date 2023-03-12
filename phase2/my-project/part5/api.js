21fbc01ef916af78a45694fe37256356d11ed695
Tom Fyfe <tomfyfe85@gmail.com>
2023-03-12 21:05:26 +0000

    working on adding types to pokedex

--- phase2/my-project/part5/api.js
+++ phase2/my-project/part5/api.js
@@ -1,12 +1,26 @@
 const fetchPokemon = (name) => {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => response.json())
-    .then((data) => ({
-      name: data.name,
-      id: data.id,
-      height: data.height,
-      weight: data.weight,
-    }));
+    .then((data) => console.log(data.types.length));
 };
+//     }),{
+//   name: data.name,
+//   id: data.id,
+//   height: data.height,
+//   weight: data.weight,
+//   type: function type() {
+//     data.types.map((type) => type.name);
+//     },
 
 module.exports = fetchPokemon;
+
+// ({
+//     name: data.name,
+//     id: data.id,
+//     height: data.height,
+//     weight: data.weight,
+//     types: data.abilities,
+//   })
+
+//   type: function() {
+//         console.log(data.types.map((type) => type.name));
