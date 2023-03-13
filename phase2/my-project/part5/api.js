ebaff600c68cd86fbd7bc3ddd2b6ddd4b5e3ac7e
Tom Fyfe <tomfyfe85@gmail.com>
2023-03-13 00:15:27 +0000

    part5 -Fininsing the Pokedex - 1 complete

--- phase2/my-project/part5/api.js
+++ phase2/my-project/part5/api.js
@@ -1,26 +1,19 @@
 const fetchPokemon = (name) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  const array = [];
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => response.json())
-    .then((data) => console.log(data.types.length));
+    .then((data) => {
+      for (let i = 0; i < data.types.length; i++) {
+        array.push(data.types[i].type.name);
+      }
+      return {
+        name: data.name,
+        id: data.id,
+        height: data.height,
+        weight: data.weight,
+        type: array,
+      };
+    });
 };
-//     }),{
-//   name: data.name,
-//   id: data.id,
-//   height: data.height,
-//   weight: data.weight,
-//   type: function type() {
-//     data.types.map((type) => type.name);
-//     },
 
 module.exports = fetchPokemon;
-
-// ({
-//     name: data.name,
-//     id: data.id,
-//     height: data.height,
-//     weight: data.weight,
-//     types: data.abilities,
-//   })
-
-//   type: function() {
-//         console.log(data.types.map((type) => type.name));
